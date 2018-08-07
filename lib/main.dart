32a582d51439bc74a683acaa1d7ee9848c6fb826
Luiz Alvarez <luizgpa@gmail.com>
2018-08-07 19:27:00 -0300

    DecorationImage with the same issue.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Image(
             image: AssetImage('assets/border.png'),
@@ -15,6 +15,14 @@ class MyApp extends StatelessWidget {
             height: 71.0,
             width: 190.0,
             repeat: ImageRepeat.repeat,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/border.png'),
+              centerSlice: Rect.fromLTRB(15.0, 15.0, 25.0, 25.0),
+            )),
+            child: SizedBox(width: 190.0, height: 71.0),
           ),
         ],
       ),
